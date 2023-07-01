48e198c22facf3b013ef79ce64b0f7617f6dce15
Gerald <adudagerald@gmail.com>
2023-07-01 19:21:38 +0300

    done

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,26 @@
 class HeroPowersController < ApplicationController
-end
+
+     ###get hero powers
+     def index 
+          hero_power = HeroPower.all 
+          render json: hero_power
+     end
+     ###making a new hero power
+     def create
+       hero = Hero.find_by(id: params[:hero_id])
+       power = Power.find_by(id: params[:power_id])
+   
+       if hero && power
+         hero_power = HeroPower.new(hero: hero, power: power, strength: params[:strength])
+   
+         if hero_power.save
+           render json: hero, include: :powers
+         else
+           render json: { errors: hero_power.errors.full_messages }
+         end
+       else
+         render json: { error: "Hero or Power not found" }
+       end
+     end
+   end
+   
