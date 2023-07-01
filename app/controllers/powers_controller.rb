eef07b844313c32d7c28c01d6ad6c0f552ca05f1
Gerald <adudagerald@gmail.com>
2023-07-01 18:27:10 +0300

    able to find power by id

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -4,4 +4,13 @@ class PowersController < ApplicationController
           powers = Power.all 
           render json: powers
      end
+     ##get powers by id
+     def show
+          powers = Power.find_by(id: params[:id])
+          if powers
+          render json: powers
+          else 
+               render json: {error: "Power not found"}
+          end
+     end
 end
