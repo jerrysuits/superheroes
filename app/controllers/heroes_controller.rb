dacb749f791daecbf33b4c61f32878c17855d3ca
Gerald <adudagerald@gmail.com>
2023-07-01 18:20:27 +0300

    able to get heroes and respective powers by id

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -4,5 +4,14 @@ class HeroesController < ApplicationController
        heroes = Hero.all
        render json: heroes
      end
+     ##get hero and respective powers by id
+     def show
+          heroes = Hero.includes(:powers).find_by(id: params[:id])
+          if heroes
+          render json: heroes, include: :powers
+          else
+               render json: {error: "Hero not found"}
+          end
+     end
 end
    
