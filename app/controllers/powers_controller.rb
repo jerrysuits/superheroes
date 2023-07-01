085d6de023b130a85be56eefa7f6e40efd3c6afe
Gerald <adudagerald@gmail.com>
2023-07-01 18:50:47 +0300

    able to update poer description

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -13,4 +13,18 @@ class PowersController < ApplicationController
                render json: {error: "Power not found"}
           end
      end
+
+     ##update description of power
+     def update
+          power = Power.find_by(id: params[:id])
+          if power
+            if power.update(description: params[:description])
+              render json: power
+            else
+              render json: { errors: power.errors.full_messages }, status: :unprocessable_entity
+            end
+          else
+            render json: { error: "Power not found" }, status: :not_found
+          end
+        end        
 end
